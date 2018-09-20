f7c2b028dcb58e4bb49735dd0260681a54700d3e
Henri Buyse <henri.buyse@gmail.com>
2018-09-20 15:52:25 +0200

    Add test on description_md

--- tests/tests_models.py
+++ tests/tests_models.py
@@ -22,3 +22,33 @@ class TestCategoryModel(TestCase):
 
     def test_verbose_name_plural(self):
         self.assertEqual(str(Category._meta.verbose_name_plural), "categories")
+
+    def test_description_md(self):
+        """Test the description_md function of post class."""
+        s = Category.objects.create(min_age=18)
+        self.assertEqual(len(s.description_md()), 0)
+
+        s = Category.objects.create(min_age=18, description="# Toto")
+        self.assertIn("<h1>", s.description_md())
+        self.assertIn("</h1>", s.description_md())
+
+        s = Category.objects.create(min_age=18, description="## Toto")
+        self.assertIn("<h2>", s.description_md())
+        self.assertIn("</h2>", s.description_md())
+
+        s = Category.objects.create(min_age=18, description="Toto")
+        self.assertIn("<p>", s.description_md())
+        self.assertIn("</p>", s.description_md())
+
+        s = Category.objects.create(min_age=18, description="*Toto*")
+        self.assertIn("<em>", s.description_md())
+        self.assertIn("</em>", s.description_md())
+
+        s = Category.objects.create(min_age=18, description="**Toto**")
+        self.assertIn("<strong>", s.description_md())
+        self.assertIn("</strong>", s.description_md())
+
+    def test_has_teams_with_trainer(self):
+        """Test the description_md function of post class."""
+        s = Category.objects.create(min_age=18)
+        self.assertFalse(s.has_teams_with_trainer())
