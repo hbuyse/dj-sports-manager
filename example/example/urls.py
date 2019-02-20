8c5bfc2b23b0065a4ad66666a99a26f79c8cff32
Henri Buyse <henri.buyse@gmail.com>
2019-02-20 01:25:23 +0100

    Correct example login logout url

--- example/example/urls.py
+++ example/example/urls.py
@@ -17,13 +17,13 @@ from django.conf import settings
 from django.conf.urls import include
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('login', auth_views.login, name="login"),
-    path('logout', auth_views.logout, name="logout"),
+    path('login', LoginView.as_view(), name="login"),
+    path('logout', LogoutView.as_view(), name="logout"),
     path('admin/', admin.site.urls, name='admin'),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('', include('sports_manager.urls', namespace='sports_manager')),
