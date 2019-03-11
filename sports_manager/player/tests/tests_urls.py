9710e59742baa6c4fd2edfe24114b848a55f1e60
Henri Buyse <henri.buyse@gmail.com>
2019-03-11 14:33:02 +0100

    Test player urls

--- sports_manager/player/tests/tests_urls.py
+++ sports_manager/player/tests/tests_urls.py
@@ -9,6 +9,29 @@ from django.utils.text import slugify
 
 
 class TestUrlsPlayer(TestCase):
-    """Tests the urls for the sports-manager."""
+    """Tests the urls for the players."""
 
-    pass
+    def test_list_url(self):
+        """Test the URL of the listing of the players."""
+        url = reverse('sports-manager:player-list', kwargs={'username': 'toto'})
+        self.assertEqual(url, '/toto/player/')
+
+    def test_create_url(self):
+        """Test the URL of that allows the creation of a player."""
+        url = reverse('sports-manager:player-create', kwargs={'username': 'toto'})
+        self.assertEqual(url, '/toto/player/create/')
+
+    def test_detail_url(self):
+        """Test the URL that gives the details of a player."""
+        url = reverse('sports-manager:player-detail', kwargs={'username': 'toto', 'slug': slugify('Hello World')})
+        self.assertEqual(url, '/toto/player/hello-world/')
+
+    def test_update_url(self):
+        """Test the URL that updates a player."""
+        url = reverse('sports-manager:player-update', kwargs={'username': 'toto', 'slug': slugify('Hello World')})
+        self.assertEqual(url, "/toto/player/hello-world/update/")
+
+    def test_delete_url(self):
+        """Test the URL that deletes a player."""
+        url = reverse('sports-manager:player-delete', kwargs={'username': 'toto', 'slug': slugify('Hello World')})
+        self.assertEqual(url, "/toto/player/hello-world/delete/")
